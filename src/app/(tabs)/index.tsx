ee1dd5c1414c308aa44ed34b11002f06ad22bf0f
Diego Fernández Criado <contact@diegofercri.dev>
2025-05-22 13:00:37 +0200

    Add GlueStack UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { dummyCompetitions } from "@/dummyData";
+import { Text, FlatList } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className='text-3xl font-thin'>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      {
+        <FlatList
+          data={dummyCompetitions}
+          renderItem={({ item }) => (
+            <Text className="text-white">{item.name}</Text>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      }
+    </GluestackUIProvider>
   );
 }
